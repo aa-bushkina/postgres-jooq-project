efae167c2ae31a28df7ed917821db75edd64fbed
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-21 07:05:01 +0300

    fix OrganizationDAOTest

--- application/src/test/java/ru/vk/DAO/OrganizationDAOTest.java
+++ application/src/test/java/ru/vk/DAO/OrganizationDAOTest.java
@@ -1,11 +1,11 @@
 package ru.vk.DAO;
 
 import com.google.inject.Inject;
+import generated.tables.records.OrganizationsRecord;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.vk.AbstractTest;
-import ru.vk.entities.Organization;
 
 import javax.inject.Named;
 import java.util.List;
@@ -25,7 +25,7 @@ class OrganizationDAOTest extends AbstractTest {
     final int uniqueId = (int) (Math.random() * 1000) + 20;
     final String uniqueInn = String.valueOf((int) (Math.random() * 1000000000) + 1000000000);
     final String uniquePaymentAccount = String.valueOf((int) (Math.random() * 1000000000) + 1000000000);
-    final Organization organization = new Organization(uniqueId, "organization", uniqueInn, uniquePaymentAccount);
+    final OrganizationsRecord organization = new OrganizationsRecord(uniqueId, "organization", uniqueInn, uniquePaymentAccount);
     organizationDAO.save(organization);
     assertThat(organizationDAO.get(uniqueId), equalTo(organization));
     organizationDAO.delete(organization);
@@ -34,22 +34,22 @@ class OrganizationDAOTest extends AbstractTest {
   @Test
   @DisplayName("Просмотр всех организаций в БД")
   void all() {
-    List<Organization> list = List.of(
-      new Organization(1, "organization1", "2330123011", "1234567890"),
-      new Organization(2, "organization2", "2500000021", "2345678901"),
-      new Organization(3, "organization3", "7300450045", "3456789012"),
-      new Organization(4, "organization4", "2201450011", "4567890123"),
-      new Organization(5, "organization5", "1050337331", "5678901234"),
-      new Organization(6, "organization6", "1050343811", "4444444444"),
-      new Organization(7, "organization7", "5444007811", "5555555554"),
-      new Organization(8, "organization8", "1050007811", "1452667778"),
-      new Organization(9, "organization9", "1052227811", "3749506837"),
-      new Organization(10, "organization10", "1054407811", "3456384888"),
-      new Organization(11, "organization11", "1050007661", "0000066666"),
-      new Organization(12, "organization12", "3567007811", "3456237474"),
-      new Organization(13, "organization13", "1052323356", "2345654334"),
-      new Organization(14, "organization14", "1056003635", "5656434564"),
-      new Organization(15, "organization15", "1023456811", "3434343434")
+    List<OrganizationsRecord> list = List.of(
+      new OrganizationsRecord(1, "organization1", "2330123011", "1234567890"),
+      new OrganizationsRecord(2, "organization2", "2500000021", "2345678901"),
+      new OrganizationsRecord(3, "organization3", "7300450045", "3456789012"),
+      new OrganizationsRecord(4, "organization4", "2201450011", "4567890123"),
+      new OrganizationsRecord(5, "organization5", "1050337331", "5678901234"),
+      new OrganizationsRecord(6, "organization6", "1050343811", "4444444444"),
+      new OrganizationsRecord(7, "organization7", "5444007811", "5555555554"),
+      new OrganizationsRecord(8, "organization8", "1050007811", "1452667778"),
+      new OrganizationsRecord(9, "organization9", "1052227811", "3749506837"),
+      new OrganizationsRecord(10, "organization10", "1054407811", "3456384888"),
+      new OrganizationsRecord(11, "organization11", "1050007661", "0000066666"),
+      new OrganizationsRecord(12, "organization12", "3567007811", "3456237474"),
+      new OrganizationsRecord(13, "organization13", "1052323356", "2345654334"),
+      new OrganizationsRecord(14, "organization14", "1056003635", "5656434564"),
+      new OrganizationsRecord(15, "organization15", "1023456811", "3434343434")
     );
     assertThat(organizationDAO.all(), equalTo(list));
   }
@@ -60,9 +60,9 @@ class OrganizationDAOTest extends AbstractTest {
     final int uniqueId = (int) (Math.random() * 1000) + 20;
     final String uniqueInn = String.valueOf((int) (Math.random() * 1000000000) + 1000000000);
     final String uniquePaymentAccount = String.valueOf((int) (Math.random() * 1000000000) + 1000000000);
-    final Organization organization = new Organization(uniqueId, "organization", uniqueInn, uniquePaymentAccount);
+    final OrganizationsRecord organization = new OrganizationsRecord(uniqueId, "organization", uniqueInn, uniquePaymentAccount);
     organizationDAO.save(organization);
-    assertThat((List<Organization>) organizationDAO.all(), hasItem(organization));
+    assertThat((List<OrganizationsRecord>) organizationDAO.all(), hasItem(organization));
     organizationDAO.delete(organization);
   }
 
@@ -72,9 +72,9 @@ class OrganizationDAOTest extends AbstractTest {
     final int uniqueId = (int) (Math.random() * 1000) + 20;
     final String uniqueInn = String.valueOf((int) (Math.random() * 1000000000) + 1000000000);
     final String uniquePaymentAccount = String.valueOf((int) (Math.random() * 1000000000) + 1000000000);
-    final Organization organization = new Organization(uniqueId, "organization", uniqueInn, uniquePaymentAccount);
+    final OrganizationsRecord organization = new OrganizationsRecord(uniqueId, "organization", uniqueInn, uniquePaymentAccount);
     organizationDAO.save(organization);
-    final Organization updatedOrganization = new Organization(uniqueId, "organization", uniqueInn, uniquePaymentAccount);
+    final OrganizationsRecord updatedOrganization = new OrganizationsRecord(uniqueId, "organization", uniqueInn, uniquePaymentAccount);
     organizationDAO.update(updatedOrganization);
     assertThat(organizationDAO.get(uniqueId), equalTo(updatedOrganization));
     organizationDAO.delete(organization);
@@ -86,10 +86,10 @@ class OrganizationDAOTest extends AbstractTest {
     final int uniqueId = (int) (Math.random() * 1000) + 20;
     final String uniqueInn = String.valueOf((int) (Math.random() * 1000000000) + 1000000000);
     final String uniquePaymentAccount = String.valueOf((int) (Math.random() * 1000000000) + 1000000000);
-    final Organization organization = new Organization(uniqueId, "organization", uniqueInn, uniquePaymentAccount);
+    final OrganizationsRecord organization = new OrganizationsRecord(uniqueId, "organization", uniqueInn, uniquePaymentAccount);
     organizationDAO.save(organization);
-    assertThat((List<Organization>) organizationDAO.all(), hasItem(organization));
+    assertThat((List<OrganizationsRecord>) organizationDAO.all(), hasItem(organization));
     organizationDAO.delete(organization);
-    assertThat((List<Organization>) organizationDAO.all(), not(hasItem(organization)));
+    assertThat((List<OrganizationsRecord>) organizationDAO.all(), not(hasItem(organization)));
   }
 }
